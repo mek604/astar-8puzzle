54dae85fdcc9298e82eaf62193b9e4c3127ef785
Mek Obchey <aobchey00@uvic.ca>
2020-07-18 15:35:53 -0700

    Added writing expansion and optimal paths to file

--- puzzle8.py
+++ puzzle8.py
@@ -14,7 +14,6 @@ def main():
 
 	s1 = "7 4 5 2 * 6 8 3 1".split(" ")
 	goal = "1 2 3 4 5 6 7 8 *".split(" ")
-
 	n = 3
 
 	# precheck
@@ -22,22 +21,32 @@ def main():
 	start = Node(s1, n, 0, 1)
 	start.calculateCost(goal)
 
-	# sanity check
+	# storing expansion and optimal path in separate files
+	out_exp = open("expansion.txt", "w")
+	out_opt = open("optimal.txt", "w")
+	openPQ = [start]
+	closedPQ = []
+
+	# sanity check to console
 	print("Goal:\n%s\n" % goal.string())
 	print("Start state:\n%s\n" % start.string())
 	print("goal allowed moves", goal.moves)
 	print("start allowed moves", start.moves)
+	# to file
+	print("Goal:\n%s\n" % goal.string(), file=out_exp)
+	print("Start state:\n%s\n" % start.string(), file=out_exp)
+	print("goal allowed moves", goal.moves, file=out_exp)
+	print("start allowed moves", start.moves, file=out_exp)
 
-	openPQ = [start]
-	closedPQ = []
 
-	print(start.cost)
 	while len(openPQ) > 0:
 		current = heappop(openPQ)
 		print("expanding cost= %d, depth= %d, board=\n%s\n" % (current.cost, current.depth, current.string()))
+		print("expanding cost= %d, depth= %d, board=\n%s\n" % (current.cost, current.depth, current.string()), file=out_exp)
 		if current.board == goal.board:
 			print("^^^^^^^^^ Found the goal state ^^^^^^^^^\n")
-			trace(current)
+			print("^^^^^^^^^ Found the goal state ^^^^^^^^^\n", file=out_exp)
+			trace(current, out_opt)
 			break
 		heappush(closedPQ, current)
 		for i in current.moves:
@@ -55,9 +64,11 @@ def main():
 					expandNode.parent = current
 					heappush(openPQ, expandNode)
 		openPQ.sort()
+	out_exp.close()
+	out_opt.close()
 
 # traceback the path from goal state to initial state
-def trace(goal):
+def trace(goal, output):
 	current = goal
 	order = []
 	while current.parent != current:
@@ -66,8 +77,10 @@ def trace(goal):
 	# add back the initial state before printing
 	order.append(current)
 	print("The optimal path to follow:\n")
+	print("The optimal path to follow:\n", file=output)
 	for i in range(len(order)-1, -1, -1):
 		print(order[i].string(), "\n")
+		print(order[i].string(), "\n", file=output)
 
 def hcost(current, goal):
 	cost = 0
