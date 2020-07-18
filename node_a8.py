667b93c197922005ff8ceb8af4f087ba3eb2817b
Mek O <mek.arnuparp@gmail.com>
2020-07-18 15:06:34 -0700

    Added manhattan distance for faster expansion

---
+++ node_a8.py
@@ -0,0 +1,89 @@
+# Node specifically implemented for A* search algorithm and 8 puzzle
+#
+# Author: Mek Obchey
+# Email: aobchey00@uvic.ca
+
+import copy
+
+class Node:
+	def __init__(self, board, size, depth, cost):
+		self.board = board
+		self.depth = depth
+		self.size = size
+		self.pos = self.findEmptyCell()	# position of the blank cell (cell with * symbol)
+		self.moves = []
+		self.cost = cost	# must be set
+		self.parent = self
+		self.__moveable__() # find valid moves
+
+	def __lt__(self, other):
+		if self.cost < other.cost:
+			return True
+		return False
+
+	def __le__(self, other):
+		if self.cost <= other.cost:
+			return True
+		return False
+
+	def copy(self):
+		return copy.deepcopy(self)
+
+	def findEmptyCell(self):
+		for i, val in enumerate(self.board):
+			if val == "*":
+				return i
+		return 0
+
+	# assuming there is only one blank cell symbol (*)
+	def string(self):
+		output = ""
+		for i, val in enumerate(self.board):
+			if i % self.size == 0 and i != 0:
+				output += "\n"
+			output += str(val) + " "
+		return output
+
+	# get a list of number movable by the empty square
+	def __moveable__(self):
+		moves = []
+		# if the position of the empty square allows the move up, down, left, right
+		if self.pos - self.size >= 0:
+			moves.append(self.pos - self.size)
+		if self.pos + self.size <= self.size * self.size - 1:
+			moves.append(self.pos + self.size)
+		if self.pos % self.size != 0:
+			moves.append(self.pos - 1)
+		if (self.pos + 1) % self.size != 0:
+			moves.append(self.pos + 1)
+		self.moves = moves
+
+	# move empty cell to position x if it is a valid position
+	def moveTo(self, x):
+		if x in self.moves and x != self.pos:
+			tmp = self.board[x]
+			self.board[x] = self.board[self.pos]
+			self.board[self.pos] = tmp
+			self.pos = x
+			self.__moveable__()
+
+	# the implementation of this function is specifically for
+	# goal state = "1 2 3 4 5 6 7 8 *"
+	# mismatch heuristic is very slow!!
+	def calculateCost(self, goal):
+		hcost = 0
+		n = self.size
+		for i, entry in enumerate(self.board):
+			if entry != "*":
+				val = int(entry) # incase board values are still interpreted as strings
+				# -------------- mismatches heuristic-----------------
+				# if val != goal.board[i]:
+				# 	hcost += 1
+				# -------------- manhattan heuristic -----------------
+				x1 = i % n
+				y1 = int((pow(n,2) - 1 - i) / n)
+				x2 = (val - 1) % n
+				y2 = int((pow(n, 2) - 1 - val) / n)
+				hcost += abs(x1 - x2) + abs(y1 - y2)
+				# -------------------------------------------
+		self.cost = hcost + self.depth
--- puzzle8.py
+++ puzzle8.py
@@ -1,18 +1,26 @@
 # A* Search Algorithm to solve 8-Puzzle problem
 # Author: Mek Obchey
+# in node_8puzzle.py, calculating the heuristic cost with 
+# manhattan distance is much much faster than calculating
+# tiles differences
+
 
 from heapq import *
-from node_8puzzle import *
-import concurrent.futures
+from node_a8 import *
 
 def main():
+	
+	# for testing only: simpler initial state and goal state
+	# s1 = "1 2 3 4 5 6 * 7 8".split(" ")
+	# s1 = "* 1 3 4 2 5 7 8 6".split(" ")
 
 	s1 = "7 4 5 2 * 6 8 3 1".split(" ")
 	goal = "1 2 3 4 5 6 7 8 *".split(" ")
+
 	n = 3
 
 	# precheck
-	goal = Node(goal, n, 0, 1) #for printing only
+	goal = Node(goal, n, 0, 0) #for printing only
 	start = Node(s1, n, 0, 1)
 	start.calculateCost(goal)
 
@@ -25,41 +33,43 @@ def main():
 	openPQ = [start]
 	closedPQ = []
 
-	k = 0
-	with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-		while len(openPQ) > 0:
-			current = heappop(openPQ)
-			print("follow cost= %d, depth= %d, board=\n%s\n" % (current.cost, current.depth, current.string()))
-			if current.board == goal.board:
-				trace(current)
-				break
-			heappush(closedPQ, current)
-			# print("expanding:\n%s\n" % current.string())
-			for i in current.moves:
-				if i != current.pos:
-					# copy its parent node and make a move
-					expandNode = current.copy()
-					expandNode.depth += 1
-					expandNode.calculateCost(goal)
-					expandNode.moveTo(i)
-					add = True
-					for nd in closedPQ:
-						if nd.board == expandNode.board:
-							add = False
-							break
-					if add:
-						expandNode.parent = current
-						heappush(openPQ, expandNode)
-			openPQ.sort()
+	print(start.cost)
+	while len(openPQ) > 0:
+		current = heappop(openPQ)
+		print("expanding cost= %d, depth= %d, board=\n%s\n" % (current.cost, current.depth, current.string()))
+		if current.board == goal.board:
+			print("^^^^^^^^^ Found the goal state ^^^^^^^^^\n")
+			trace(current)
+			break
+		heappush(closedPQ, current)
+		for i in current.moves:
+			if i != current.pos:
+				expandNode = current.copy()
+				expandNode.depth += 1
+				expandNode.calculateCost(goal)
+				expandNode.moveTo(i)
+				add = True
+				for nd in closedPQ:
+					if nd.board == expandNode.board:
+						add = False
+						break
+				if add:
+					expandNode.parent = current
+					heappush(openPQ, expandNode)
+		openPQ.sort()
 
+# traceback the path from goal state to initial state
 def trace(goal):
 	current = goal
 	order = []
 	while current.parent != current:
 		order.append(current)
 		current = current.parent
-	for i in order:
-		print(i.string())
+	# add back the initial state before printing
+	order.append(current)
+	print("The optimal path to follow:\n")
+	for i in range(len(order)-1, -1, -1):
+		print(order[i].string(), "\n")
 
 def hcost(current, goal):
 	cost = 0
@@ -67,22 +77,6 @@ def hcost(current, goal):
 		if val != "*" and val != goal.board[i]:
 			cost += 1
 	return cost
-
-def printPuzzle(string, puzzle):
-	formatPuzzle(puzzle)
-	print(string)
-	for i, val in enumerate(puzzle):
-		if i % 3 == 0 and i != 0:
-			print("")
-		print("%s " % str(val), end="")
-	print("\n")
-
-# interpret non puzzle entries
-def formatPuzzle(puzzle):
-	for i, val in enumerate(puzzle):
-		if val == "*":
-			puzzle[i] = 0
-		puzzle[i] = int(puzzle[i])
 
 if __name__ == "__main__":
 	main()
